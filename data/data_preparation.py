7637d503e7e3775612bb268549207f0295136f03
Javier Mondragon <javierus@javiers-mbp.home>
2024-03-05 20:39:11 +0100

    add functions

--- data/data_preparation.py
+++ data/data_preparation.py
@@ -1,26 +1,58 @@
 import pandas as pd
 import requests
-import os
+from bs4 import BeautifulSoup
+import gzip
+import json
+import re
 
+def load_data(
+    file_name: str, 
+    head: int = 500
+) -> pd.DataFrame:
+    file_path = f"datasets/{file_name}"
+    file_extension = file_name.split(".")[-1]
+    if file_name.split(".")[-1] == "csv":
+        df = pd.read_csv(file_path, nrows=head)
+    elif file_extension == "json":
+        try:
+            df = pd.read_json(file_path, lines=True, orient='records', nrows=head)
+        except:
+            df = pd.DataFrame(fix_json(file_path, head))
+    else:
+        raise Exception(f"Format of dataset {file_name} not loadable.")
+    return df
 
-def download_by_name(
-    project: str, 
-    fname: str, 
-    base_url: str = "https://datarepo.eng.ucsd.edu/mcauley_group/gdrive"
-) -> None:
-    """Downloads a specific dataset from a project by filename"""
-    file_path = f"data/datasets/{project}/{fname}"
-    if os.path.exists(file_path):
-        print(f"Dataset {fname} already exists")
-        return
-    url = f"{base_url}/{project}/{fname}"
-    try:
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            with open(file_path, "wb") as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-    except Exception as e:
-        print(f"There was a problem downloading the dataset {fname}: {e}")
-        return
-    print(f"Dataset {fname} has been downloaded.")
+def fix_json(file_path: str, head: int | None) -> list:
+    count = 0
+    results = []
+    with open(file_path, "r") as file:
+        for line in file:
+            modified_line = extract_specific_values(line)
+            results.append(modified_line)
+            count += 1
+            if head is not None and count > head:
+                break
+    return results 
+
+def extract_specific_values(s: str) -> dict:
+    extracted_values = {}
+    patterns = {
+        'userId': r"'userId':\s*'([^']*)'",
+        'stars': r"'stars':\s*([\d.]+)",
+        'itemId': r"'itemId':\s*'([^']*)'",
+    }
+    for key, pattern in patterns.items():
+        match = re.search(pattern, s)
+        if match:
+            extracted_values[key] = match.group(1)  
+    return extracted_values
+
+def get_genreline_divs(itemId: str) -> list:
+    url = f"https://www.librarything.com/work/{itemId}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        genreline_divs = soup.find_all("div", class_="genreline")       
+        return [div.text for div in genreline_divs]
+    else:
+        raise ConnectionError(f"Satus code: {response.status_code}, message: {response.text}")
