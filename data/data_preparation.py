843a1da90fdd6e3cd2ce0d21033e2866b0f04a80
Javier Mondragon <javierus@Javiers-MacBook-Pro.local>
2024-03-07 15:47:24 +0100

    update vectors

--- data/data_preparation.py
+++ data/data_preparation.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import requests
+import numpy as np
 from bs4 import BeautifulSoup
+from collections import defaultdict
 import gzip
 import json
 import re
@@ -56,3 +58,51 @@ def get_genreline_divs(itemId: str) -> list:
         return [div.text for div in genreline_divs]
     else:
         raise ConnectionError(f"Satus code: {response.status_code}, message: {response.text}")
+
+def get_model_dataframe(n_users: int, sample_users: int) -> pd.DataFrame:
+    df_users_raw = load_data("goodreads/goodreads_interactions.csv", n_users)
+    user_ids_sample = df_users_raw["user_id"].sample(sample_users)
+    df_users_filtered = df_users_raw.loc[df_users_raw["user_id"].isin(user_ids_sample)]
+    df_items_raw = load_data("goodreads/goodreads_book_genres_initial.json", None)
+    unique_item_ids = df_users_filtered["book_id"].unique().tolist()
+    df_items_filtered = df_items_raw.loc[df_items_raw["book_id"].isin(unique_item_ids)]
+    df_items_filtered["genres"] = df_items_filtered.loc[:, "genres"].apply(get_genres_from_dict)
+    df_combined = pd.merge(df_users_filtered, df_items_filtered, how="left", on="book_id")
+    return df_combined.dropna(subset=["genres"]).loc[df_combined["is_read"] == 1]
+
+def get_genres_from_dict(d: dict) -> dict:
+    genres = {}
+    all_votes = 0
+    for genre, value in d.items():
+        for g in genre.split(","):
+            genres.update({g.strip(): value})
+            all_votes += value
+    if genres and all_votes:
+        return {k: v / all_votes for k, v in genres.items()}
+    return {}
+
+def get_category_vector(df: pd.DataFrame) -> list:
+    unique_genres = set()
+    for _, row in df.iterrows():
+        if isinstance(row["genres"], dict):
+            unique_genres.update(row["genres"].keys())
+    return list(unique_genres)
+
+def get_users_vectors(df: pd.DataFrame, vector: list) -> pd.DataFrame:
+    users = df["user_id"].unique().tolist()
+    results = pd.DataFrame(columns=vector)
+    results.index.name = "user_id"
+    for user in users:
+        weighted_sums = defaultdict(float)
+        counts = defaultdict(int)
+        tmp_df = df.loc[df["user_id"] == user]
+        for _, row in tmp_df.iterrows():
+            for k, v in row["genres"].items():
+                weighted_sums[k] += v * row["rating"] / 5
+                counts[k] += 1
+        user_vector = {key: weighted_sums[key] / counts[key] for key in weighted_sums}
+        user_vector_total = sum(user_vector.values())
+        user_vector = {key: user_vector[key] / user_vector_total for key in user_vector}
+        results.loc[user] = user_vector
+    return results
+
--- model/model.py
+++ model/model.py
@@ -1,14 +1,19 @@
 import mesa
 from agents import ItemAgent, UserAgent
+from data.data_preparation import get_model_dataframe
 
 
 class RecommenderSystemModel(mesa.Model):
     
-    def __init__(self, n_items, n_users):
+    def __init__(
+        self, 
+        n_users: int,
+        dataset: str
+    ):
         super().__init__()
-        self.num_items = n_items
         self.num_users = n_users
         self.schedule = mesa.time.RandomActivation(self)
+        df = get_model_dataframe(dataset, n_users)
         for i in range(self.num_agents):
             a = ItemAgent(i, self)
             self.schedule.add(a)
