bf5299b986729abe8a4a0065a37d21829c64906d
Javier Mondragon <javierus@javiers-mbp.kpn>
2024-05-01 12:38:41 +0200

    update

--- project/utils.py
+++ project/utils.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+from typing import Any
 
 
 def string_to_array(s):
@@ -19,3 +21,46 @@ def plot_agent_vector(df: pd.DataFrame, agent_id: int) -> None:
         plt.legend()
         plt.grid(False)
         plt.show()
+
+def plot_vector_diffs(df: pd.DataFrame, model: str) -> None:
+    ax = df["vector_diff"].hist(
+        bins=20, 
+        grid=False, 
+        edgecolor="black"
+    )
+    median_value = df["vector_diff"].median()
+    plt.axvline(median_value, color="r", linestyle="dotted", linewidth=2, label=f"Median: {median_value:.6f}")
+    plt.xlabel("Cosine similarity")
+    plt.ylabel("Frequency")
+    plt.title(f"Histogram of cosine similarity between first and last vector per user ({model})")
+    plt.legend()
+    plt.show()
+
+def list_file_paths(directory: str) -> list:
+    file_paths = []
+    for root, _, files in os.walk(directory):
+        for filename in files:
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+    return file_paths
+
+def get_value_from_results(df: pd.DataFrame, id: int, col_name: str, step: int | None = None) -> Any:
+    if "AgentID" not in df.columns:
+        filtered_df = df[df["unique_id"] == id]
+        return filtered_df[col_name].iloc[0]
+    filtered_df = df[df["AgentID"] == id]
+    if step == 0:
+        row = filtered_df[filtered_df["Step"] == filtered_df["Step"].min()]
+    elif step == -1:
+        row = filtered_df[filtered_df["Step"] == filtered_df["Step"].max()]
+    else:
+        row = filtered_df[filtered_df["Step"] == step]
+    return row[col_name].iloc[0]
+
+def normalize_vector(vector: np.ndarray | str, as_percentage: bool = False) -> np.ndarray:
+    if isinstance(vector, str):
+        vector = string_to_array(vector)
+    total_sum = vector.sum()
+    percentage = 100 if as_percentage else 1
+    result = vector * percentage / total_sum if total_sum > 0 else 0
+    return result
